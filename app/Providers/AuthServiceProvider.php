c85f33dac04927a7fd2161c97ac35618a699d83d
Vincent Nguyen <mr.vannguyen92@gmail.com>
2018-08-22 15:18:21 +0700

    Setting basic ACL

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -2,7 +2,9 @@
 
 namespace App\Providers;
 
+use App\Permission;
 use Illuminate\Support\Facades\Gate;
+use Illuminate\Support\Facades\Schema;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 
 class AuthServiceProvider extends ServiceProvider
@@ -25,6 +27,17 @@ class AuthServiceProvider extends ServiceProvider
     {
         $this->registerPolicies();
 
-        //
+        if (Schema::hasTable('permissions')) {
+            foreach ($this->getPermissions() as $permission) {
+                Gate::define($permission->name, function ($user) use($permission) {
+                    return $user->hasRole($permission->roles);
+                });
+            }
+        }
+    }
+
+    protected function getPermissions()
+    {
+        return Permission::with('roles')->get();
     }
 }
--- app/User.php
+++ app/User.php
@@ -2,6 +2,7 @@
 
 namespace App;
 
+use App\Role;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 
@@ -26,4 +27,36 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
+
+    public function roles()
+    {
+        return $this->belongsToMany('App\Role');
+    }
+
+    public function hasRole($role)
+    {
+        if (is_string($role)) {
+            return $this->roles->contains('name', $role);
+        }
+
+        return $role->intersect($this->roles)->count() ? true : false;
+
+        // foreach ($role as $r) {
+        //     if ($this->hasRole($r->name)) {
+        //         return true;
+        //     }
+        // }
+
+        // return false;
+    }
+
+    public function assign($role)
+    {
+        if (is_string($role)) {
+            return $this->roles()->save(
+                Role::whereName($role)->firstOrFail();
+            );
+        }
+        return $this->roles()->save($role);
+    }
 }
